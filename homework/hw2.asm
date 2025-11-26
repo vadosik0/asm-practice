456a86758be7b4bb5e366f0655e836c97931b466
vadosik0 <166757830+vadosik0@users.noreply.github.com>
2025-11-26 21:14:16 +0200

    пр2

---
+++ homework/hw2.asm
@@ -0,0 +1,17 @@
+implement function IntToString
+
+int2string(x: Int) -> String
+char[] int2string(int x)
+
+
+_start:
+         mov eax, 1234567
+         mov esi, buffer
+         call int2str  
+
+
+
+
+-
+-
+-
